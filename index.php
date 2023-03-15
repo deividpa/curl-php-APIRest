a9b128962f3b8db036ac45f149c6acb6b08f5fce
DavidPerezSmart <david.perez@smartalentit.com>
2023-03-15 14:50:13 -0500

    Update index.php
    Se finaliza la solicitud GET a la API por medio de cURL decodificando la respuesta en un formato json.

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
+
+    //Petición GET a una API
+
     $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL,"https://catfact.ninja/fact");
-    curl_exec($ch);
+    curl_setopt($ch, CURLOPT_URL,"https://catfact.ninja/fact");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
     if(curl_errno($ch)) {
         echo curl_error($ch);
+    } else {
+        $decoded = json_decode($response, true);
+        var_dump($decoded);
     }
+    
     curl_close($ch);
